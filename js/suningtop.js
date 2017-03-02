6451bd0253af82349acae0f49a240b7a3c2efa5f
duxingyu <duxy1995@163.com>
2017-03-02 22:36:43 +0800

    与苏宁首页相关CSS/JS/img等

---
+++ js/suningtop.js
@@ -0,0 +1,26 @@
+function jsonp(link) {
+  const sc = document.createElement('script');
+  sc.src = `${link}&_=${+new Date}`;
+  document.body.appendChild(sc);
+}
+jsonp('http://ds.suning.cn/ds/hotkeywords/0--showHotkeywords.xjsonp?callback=showHotkeywords');
+
+function showHotkeywords(data) {
+  document.getElementById('serach_key').innerHTML = data.html;
+  document.querySelector('#serach_inp input').placeholder = document.getElementById('searchDefaultKeyword').value;
+}
+
+function getHotkey() {
+  jsonp('http://ds.suning.cn/ds/searchHotkeywords/0-searchHotkeywords.jsonp?callback=searchHotkeywords');
+  this.removeEventListener('focus', getHotkey, false);
+}
+document.getElementById('Serach').addEventListener('focus', getHotkey, false);
+
+function searchHotkeywords(data) {
+  const oDl = document.getElementById('hot_list');
+  let ohtml = '';
+  data.forEach((val) => {
+    ohtml += `<dd><a href="http://www.serach.suning.com/${val.urlKeyword}/">${val.keyword}</a></dd>`;
+  });
+  oDl.innerHTML += ohtml;
+}
